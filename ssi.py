93ba440061f01ef714505f375511acc2c019be0d
Edward Knueppel <edward.knueppel@zywave.com>
2022-09-17 09:50:06 -0500

    Gnomes example

---
+++ ssi.py
@@ -0,0 +1,16 @@
+gnomes = ["Teddy", "Frank", "Lisa"]
+
+
+print(gnomes[0])
+
+for gnome in gnomes:
+    print(gnome)
+
+while gnomes[0] == "Teddy":
+    print(gnomes[0])
+    gnomes[0] = "Kim"
+
+for best in range(10):
+    print(best)
+
+name = input("What is your name friend?")
